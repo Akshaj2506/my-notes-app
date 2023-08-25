4e8fabf535b6ed614a6debac0b66cee7e1f3dbc3
Akshaj2506 <74644487+Akshaj2506@users.noreply.github.com>
2023-08-25 19:08:18 +0530

    added validation to data which is getting stored in database

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,29 @@
 const express = require('express');
+const { body, validationResult } = require('express-validator');
 const User = require('../models/User');
 const router = express.Router();
 
-router.post('/create',(req, res) => {
-   const user = User(req.body);
-   user.save();
-   res.json(req.body);
-});
+router.post('/create',
+   [
+      body('name').isLength({ min: 4 }),
+      body('email').isEmail(),
+      body('password').isStrongPassword()
+   ]
+   , (req, res) => {
+      const errors = validationResult(req);
+      if (!(errors.isEmpty())) {
+         res.status(400).json({ errors: errors.array() });
+      } else {
+         const { name, email, password, date} = req.body;
+         User.create({
+            name : name,
+            email : email,
+            password : password,
+            date : date
+         }).then(user => res.json(user))
+         .catch(err => res.json({error : err.message}));
+      }
+   }
+);
 
 module.exports = router;
