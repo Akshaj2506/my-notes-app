578b356518224d90571efe43e133a335156fcb3c
Akshaj2506 <74644487+Akshaj2506@users.noreply.github.com>
2023-09-05 14:24:23 +0530

    added validation for email

--- backend/db.js
+++ backend/db.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 const mongoURI = 'mongodb://localhost:27017/myNotesApp';
 
 const connectToMongo = () => {
-   mongoose.connect(mongoURI);
-   console.log("Connected to database");
+   mongoose.connect(mongoURI).then(console.log("Connected to database"));
 }
 
 module.exports = connectToMongo;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,23 +5,34 @@ const router = express.Router();
 
 router.post('/create',
    [
-      body('name').isLength({ min: 4 }),
-      body('email').isEmail(),
-      body('password').isStrongPassword()
+      body('name', "Minimum length of name should be 4 characters").isLength({ min: 4 }),
+      body('email', "Wrong format of email entered").isEmail(),
+      body('password', "Password is not strong enough").isStrongPassword()
    ]
-   , (req, res) => {
+   , async (req, res) => {
+      // Checking for any sort of errors in the request, return errors
       const errors = validationResult(req);
       if (!(errors.isEmpty())) {
          res.status(400).json({ errors: errors.array() });
       } else {
+         // Checking if the user already exists
+         const user = await User.findOne({email : req.body.email});
+         if (user) {
+            return res.status(400).json({error: "User already exists"});
+         }
+         // Create user if no issues found
          const { name, email, password, date} = req.body;
-         User.create({
-            name : name,
-            email : email,
-            password : password,
-            date : date
-         }).then(user => res.json(user))
-         .catch(err => res.json({error : err.message}));
+         try {
+            const createdUser = await User.create({
+               name : name,
+               email : email,
+               password : password,
+               date : date
+            })
+            res.json(createdUser)
+         } catch(err) {
+            res.status(500).json({ error: err.message });
+         }
       }
    }
 );
