413a44bf9576ec2ad62eafc88b64afde0fc29e8d
Akshaj2506 <74644487+Akshaj2506@users.noreply.github.com>
2023-09-11 03:48:42 +0530

    created an API endpoint for updating a note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,22 +1,22 @@
 const express = require('express');
-const {body, validationResult} = require('express-validator');
+const { body, validationResult } = require('express-validator');
 const fetchUser = require('../middleware/fetchuser');
 const router = express.Router();
 const Note = require('../models/Note');
 
-router.post('/createnote', fetchUser,[
-   body('title', 'Title should be atleast 3 characters').isLength({ min: 3}),
-   body('description', "The description should be at least 10 characters").isLength({min : 10})
+router.post('/createnote', fetchUser, [
+   body('title', 'Title should be atleast 3 characters').isLength({ min: 3 }),
+   body('description', "The description should be at least 10 characters").isLength({ min: 10 })
 ], async (req, res) => {
    try {
       const errors = validationResult(req);
       if (!(errors.isEmpty())) {
-         res.status(400).json({errors : errors.array()});
+         res.status(400).json({ errors: errors.array() });
       } else {
-         const {title, description, tag} = req.body;
+         const { title, description, tag } = req.body;
          const createdNote = await Note.create({
-            user : req.user.id,
-            title : title,
+            user: req.user.id,
+            title: title,
             description: description,
             tag: tag
          })
@@ -24,20 +24,38 @@ router.post('/createnote', fetchUser,[
       }
    } catch (error) {
       console.error(error.message);
-      res.status(401).json({error : "Internal Server Error"});
+      res.status(500).json({ error: "Internal Server Error" });
    }
-   
 })
 
 router.get('/fetchallnotes', fetchUser, async (req, res) => {
    try {
-      const notes = await Note.find({user : req.user.id});
-      res.json({notes: notes})
+      const notes = await Note.find({ user: req.user.id });
+      res.json({ notes: notes })
    } catch (error) {
       console.error(error.message);
-      res.status(401).json({error : "Internal Server Error"});
+      res.status(500).json({ error: "Internal Server Error" });
    }
+})
 
+router.put('/updatenote/:id', fetchUser, async (req, res) => {
+   try {
+      const {title, description, tag} = req.body;
+      const newNote = {};
+      if (title) newNote.title = title;
+      if (description) newNote.description = description;
+      if (tag) newNote.tag = tag;
+
+      const note = await Note.findById(req.params.id)
+      if (!note) res.status(404).json({error: "Note not found"});
+      if (req.user.id !== note.user.toString()) res.status(401).json({error : "Access not granted"});
+
+      const updatedNote = await Note.findByIdAndUpdate(req.params.id, {$set : newNote}, {new: true});
+      res.json(updatedNote);
+   } catch (error) {
+      console.error(error.message);
+      res.status(500).json({ error: "Internal Server Error" });
+   }
 })
 
 module.exports = router;
